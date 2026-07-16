00402f1d9e477566c5ced04968e5de9c1d46f5c3
Alexander Guyer <guyera@oregonstate.edu>
2026-07-16 14:42:58 -0700

    Site

--- site/index.js
+++ site/index.js
@@ -2,4 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const loadingContent = document.getElementById("loading-content");
     const acceptAssignmentContent = document.getElementById("accept-assignment-content");
     console.log('HTML fully loaded and parsed');
+
+    loadingContent.style.display = "none";
+    acceptAssignmentContent.style.display = "block";
 });
