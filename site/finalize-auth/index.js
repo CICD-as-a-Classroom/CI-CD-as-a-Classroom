7133d06777b125dcf3499a9c1e85f503fcb56d0f
Alexander Guyer <guyera@oregonstate.edu>
2026-07-22 16:09:44 -0700

    Site

--- site/finalize-auth/index.js
+++ site/finalize-auth/index.js
@@ -334,9 +334,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     setCookie('accessToken', accessToken);
     setCookie('refreshToken', refreshToken);
 
-    console.log(`state: ${state}`);
-    console.log(`originating URL: ${state.originatingUrl}`);
-
     // Redirect user back to where they were when auth flow started
-    //window.location.replace(state.originatingUrl);
+    window.location.replace(state['originatingUrl']);
 });
