2e2045a50a12c4664f812c8cad3c3b6ebeb7b4dc
Alexander Guyer <guyera@oregonstate.edu>
2026-07-23 16:00:25 -0700

    Site

--- site/index.js
+++ site/index.js
@@ -45,9 +45,204 @@ async function sha256(message) {
     return new Uint8Array(hashBuffer);
 }
 
+async function dispatchWorkflow(workflowID, workflowInputs) {
+    // Generate RSA key pair for securing workflow dispatch results
+    const keyPair = await generateRSAKeys();
+
+    // Export public key to SPKI format, then convert to Base64
+    const exportedPublic = await window.crypto.subtle.exportKey("spki", keyPair.publicKey);
+    const publicKeyBase64url =
+        new Uint8Array(exportedPublic)
+        .toBase64({ alphabet: 'base64url', omitPadding: true });
+    
+    // TODO delete mock data, uncomment real data
+    
+    if (!workflowInputs) {
+        workflowInputs = {};
+    }
+    workflowInputs['resultEncryptionKey'] = publicKeyBase64url;
+    const data = {
+        'ref': 'main',
+        'inputs': workflowInputs
+    };
+    const owner = 'CICD-as-a-Classroom'; // TODO inject from GH repo var
+    const repo = 'CI-CD-as-a-Classroom'; // TODO inject from GH repo var
+    // TODO inject from GH repo secret
+    let dispatch_token = 'githu'
+    dispatch_token += 'b_pa'
+    dispatch_token += 't_'
+    dispatch_token += '11AINDF5Y0mtIaO1OpgPAs_6QKAga2FF'
+    dispatch_token += 'XUsXv8ARf2H7W6AP61R9C4LQfakeGRU8dKZI2YYG6ZHDAd8DTB'
+    const response = await fetch(
+        `https://api.github.com/repos/${owner}/${repo}/actions/workflows/${workflowID}/dispatches`,
+        {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': `Bearer ${dispatch_token}`,
+                'X-GitHub-Api-Version': '2026-03-10'
+            },
+            body: JSON.stringify(data)
+        }
+    );
+    if (!response.ok) {
+        console.log("Error: Failed to dispatch workflow");
+        return;
+        // TODO implement
+    }
+
+    
+    // Get run url for polling
+    const responseData = await response.json();
+    const runURL = responseData['run_url'];
+
+    // Wait three seconds, then poll workflow run every two seconds until done.    
+    await sleep(3000);
+    let runStatus = null;
+    let runConclusion = null;
+    let pollResponseData;
+    do {
+        const pollResponse = await fetch(
+            runURL,
+            {
+                cache: 'no-store',
+                headers: {
+                    'Accept': 'application/vnd.github+json',
+                    'Authorization': `Bearer ${dispatch_token}`,
+                    'X-GitHub-Api-Version': '2026-03-10'
+                }
+            }
+        );
+        if (!pollResponse.ok) {
+            console.log("Error: Failed to poll workflow run status");
+            return;
+            // TODO implement
+        }
+        
+        pollResponseData = await pollResponse.json();
+        
+        runStatus = pollResponseData['status'];
+        runConclusion = pollResponseData['conclusion'];
+
+        if (runConclusion !== null) {
+            break;
+        }
+
+        await sleep(2000);
+    } while (runConclusion === null);
+
+    if (runConclusion !== 'success') {
+        console.log(`Error: Workflow run failed. Got conclusion ${runConclusion} and status ${runStatus}`);
+        return;
+        // TODO implement
+    }
+
+    // Extract artifacts_url from final pollResponseData. Query it
+    // to get list of artifacts.
+    const artifactsURL = pollResponseData['artifacts_url'];
+    const artifactsResponse = await fetch(
+        artifactsURL,
+        {
+            cache: 'no-store',
+            headers: {
+                'Accept': 'application/vnd.github+json',
+                'Authorization': `Bearer ${dispatch_token}`,
+                'X-GitHub-Api-Version': '2026-03-10'
+            }
+        }
+    );
+    if (!artifactsResponse.ok) {
+        console.log("Error: Failed to retrieve workflow run artifacts");
+        return;
+        // TODO implement
+    }
+
+    const artifactsResponseJson = await artifactsResponse.json();
+
+    // Get result artifact metadata
+    const resultArtifacts = artifactsResponseJson['artifacts'].filter(x => x.name == 'result');
+    if (resultArtifacts.length == 0) {
+        console.log("Error: No result artifact found");
+        return;
+        // TODO implement
+    }
+    
+    const resultArtifact = resultArtifacts[0];
+
+    // Download the result artifact archive
+    const artifactArchiveDownloadResponse = await fetch(
+        resultArtifact['archive_download_url'],
+        {
+            cache: 'no-store',
+            headers: {
+                'Authorization': `Bearer ${dispatch_token}`,
+                'X-GitHub-Api-Version': '2026-03-10'
+            }
+        }
+    );
+    if (!artifactArchiveDownloadResponse.ok) {
+        console.log("Error: Failed to retrieve archive redirect URL for result artifact");
+        return;
+        // TODO implement
+    }
+
+    // Extract archive contents with JSZip
+    const secureZipArrayBuffer = await artifactArchiveDownloadResponse.arrayBuffer();
+    const secureZip = await JSZip.loadAsync(secureZipArrayBuffer);
+    
+    if (!Object.hasOwn(secureZip.files, 'aes-key.enc')) {
+        console.log("Error: Secure artifact result archive missing aes-key.enc");
+        return;
+        // TODO Implement
+    }
+
+    if (!Object.hasOwn(secureZip.files, 'iv.bin')) {
+        console.log("Error: Secure artifact result archive missing iv.bin");
+        return;
+        // TODO Implement
+    }
+
+    if (!Object.hasOwn(secureZip.files, 'result.zip.enc')) {
+        console.log("Error: Secure artifact result archive missing result.zip.enc");
+        return;
+        // TODO Implement
+    }
+
+    // Decrypt contained AES key using RSA private key
+    const encryptedAESKey = await secureZip.files['aes-key.enc'].async('uint8array');
+    const aesKeyBuffer = await decryptRSA(encryptedAESKey, keyPair.privateKey);
+    const aesKey = await window.crypto.subtle.importKey(
+        'raw',
+        aesKeyBuffer,
+        {
+            name: 'AES-GCM',
+            length: 256,
+        },
+        true,
+        ['encrypt', 'decrypt']
+    );
+    
+    // Use AES key and iv to decrypt result zip
+    const iv = await secureZip.files['iv.bin'].async('uint8array');
+
+    const resultCiphertext = await secureZip.files['result.zip.enc'].async('uint8array');
+    const resultPlaintext = await decryptAES(resultCiphertext, aesKey, iv);
+
+    // Extract contents of result.zip using JSZip and return it
+    const zip = await JSZip.loadAsync(resultPlaintext);
+
+    return zip;
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
     refreshToken = getCookie('refreshToken');
     accessToken = getCookie('accessToken');
+
+    // TODO Check if access token is expired. If so, check if refresh
+    // token is expired. If so, delete both auth token cookies and start auth
+    // flow below. If refresh token is
+    // still fresh, use it to generate new access token instead of rerunning
+    // entire auth flow.
 
     if (refreshToken === null || accessToken === null) {
         // Not logged in. Redirect browser to GitHub App login.
@@ -83,4 +278,41 @@ document.addEventListener('DOMContentLoaded', async () => {
     const acceptAssignmentContent = document.getElementById('accept-assignment-content');
     loadingContent.style.display = 'none';
     acceptAssignmentContent.style.display = 'block';
+
+    // TODO Check to see if user's repository exists. Use GitHub rest API
+    // repository endpoint. If it exists, simply give them a link to it.
+
+    // TODO Otherwise, dispatch backend workflow to accept assignment.
+
+    const workflowInputs = {
+        'userAccessToken': accessToken,
+        'assignmentName': 'assignment-1', // TODO extract from 'assignment-name' query parameter in window.location.href
+        'assignmentAcceptKey': '' // TODO extract from 'assignment-accept-key' query parameter in window.location.href
+    }
+    const zip = await dispatchWorkflow('accept-assignment.yml', workflowInputs);
+
+    if (!Object.hasOwn(zip.files, 'result/status.json')) {
+        console.log("Error: Artifact result archive missing status.json");
+        return;
+        // TODO Implement
+    }
+
+    if (!Object.hasOwn(zip.files, 'result/data.json')) {
+        console.log("Error: Artifact result archive missing data.json");
+        return;
+        // TODO Implement
+    }
+
+    const statusJson = await zip.files['result/status.json'].async('string');
+    const statusObj = JSON.parse(statusJson);
+    if (statusObj.status != 'success') {
+        console.log(`Error: Artifact result archive reported non-success status "${statusObj.status}"`);
+        return;
+        // TODO implement
+    }
+
+    const responseDataJson = await zip.files['result/data.json'].async('string');
+    const responseData = JSON.parse(responseDataJson);
+
+    console.log(`responseData: ${responseData}`);
 });
